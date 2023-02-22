b53ea883ec0d11c42be39f2cb4de40fb454eb9af
AbylaiNur <93826390+AbylaiNur@users.noreply.github.com>
2023-02-22 21:32:14 +0600

    feat: add image muter

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
-const multer = require('multer');
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
@@ -10,21 +9,23 @@ const ejsMate = require('ejs-mate')
 const authMiddleware = require('./middlewares/authMiddleware.js');
 const refreshTokenMiddleware = require('./middlewares/refreshTokenMiddleware.js');
 const cookieParser = require('cookie-parser');
+const imageRouter = require('./routes/imageRouter');
+const morgan = require('morgan');
 
 const app = express();
-const upload = multer();
 const PORT = process.env.PORT || 5000;
-const mongoURI = "mongodb+srv://bekatop1gg:GGgsF9ZGc4R59Rfs@cars.dg4sc3h.mongodb.net/Janym"
+const mongoURI = "mongodb+srv://bekatop1gg:GGgsF9ZGc4R59Rfs@cars.dg4sc3h.mongodb.net/Janym";
 
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
 
 app.use('/styles', express.static(path.join(__dirname, '/styles')));
 app.use('/images', express.static(path.join(__dirname, '/images')));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(upload.array());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // DON'T CHANGE THE ORDER OF THESE MIDDLEWARES
@@ -33,6 +34,7 @@ app.use(authMiddleware);
 
 app.use(mainRoutes);
 app.use('/auth', authRouter);
+app.use('/upload', imageRouter);
 
 async function start() {
     try {
---
+++ routes/imageRouter.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const imgModel = require("../models/image");
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+const process = require("process");
+const directory = path.join(__dirname + "/../uploads/");
+console.log(directory);
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    fs.mkdirSync(directory, { recursive: true });
+    cb(null, directory);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/", (req, res) => {
+  imgModel.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("An error occurred", err);
+    } else {
+      res.render("imagesPage", { items: items });
+    }
+  });
+});
+
+router.use((req, res, next) => {
+    console.log("Hello, World!");
+    next();
+});
+
+router.post("/", upload.single("image"), (req, res, next) => {
+  console.log("Inside post controller");
+  const obj = {
+    name: req.body.name,
+    desc: req.body.desc,
+    img: {
+      data: fs.readFileSync(
+        path.join(directory + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+  console.log("Succesfully uploaded");
+  imgModel.create(obj, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("./");
+    }
+  });
+});
+
+module.exports = router;
